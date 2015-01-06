b8d8b74242bac507996f512c6dc1318b5cb941ea
Jesper Skole <jesper@codex-deesign.com>
2015-01-06 15:04:19 +0100

    Changes from school

---
+++ MoveAttack.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 492719
+ * Date: 06-01-2015
+ * Time: 13:01
+ */
+
+class MoveAttack {
+
+
+
+    public function __construct($side1, $side2, $side3) {
+
+
+    }
+
+    public function resultat() {
+        return $this->side1 . $this->side2 . $this->side3;
+    }
+
+}
+
